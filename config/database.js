0655b6af17eb482896c2a5726bffa1e0fe2d0d52
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-11-15 00:03:11 +0700

    database changes

--- config/database.js
+++ config/database.js
@@ -1,8 +1,5 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("backend_fullstack", "root", "", {
-    host:"localhost",
-    dialect:"postgres"
-});
+const db = new Sequelize("postgres://dzkokplxmwbcix:8b950cfa601103d6372ba176a66e4bf9e8dec2ed18f7a0d579a60f5201a19a22@ec2-52-207-90-231.compute-1.amazonaws.com:5432/d2veulsjfo91cj");
 
-export default db;
+export default db; 
