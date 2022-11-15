93ae5e55e4df7607c42655cd5945fdec79e3f26b
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-11-15 14:18:40 +0700

    cors

--- route/api.js
+++ route/api.js
@@ -2,11 +2,13 @@ import express from "express"
 import { getUsers, RegisterUsers, Login, Logout} from "../controller/UserController.js";
 import { verifyToken } from "../middleware/auth.js";
 import { refreshToken } from "../controller/RefreshToken.js";
+import { Cors } from "../middleware/Cors.js";
+
 const Routes = express.Router();
 
 Routes.get('/users', verifyToken, getUsers);
-Routes.post('/users', RegisterUsers);
-Routes.post('/login', Login);
+Routes.post('/users', Cors, RegisterUsers);
+Routes.post('/login', Cors, Login);
 Routes.get('/token', refreshToken);
 Routes.delete('/logout', Logout);
 
