4786d8664d91ad97e72ea688ebf2a5920d403c6a
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-11-15 13:25:46 +0700

    cors\

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ dotenv.config();
 
 const app = express();
 const port  = process.env.PORT || 8000;
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://ap1-backend.herokuapp.com/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(cors({
     origin: '*'
 })); 
