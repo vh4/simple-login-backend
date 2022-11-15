0bd06335948c188dc83f5e23f20ee3a334578f1a
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-11-15 14:58:21 +0700

    cors v1

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("d2veulsjfo91cj", "dzkokplxmwbcix", "8b950cfa601103d6372ba176a66e4bf9e8dec2ed18f7a0d579a60f5201a19a22", {
-    host:"postgres://dzkokplxmwbcix:8b950cfa601103d6372ba176a66e4bf9e8dec2ed18f7a0d579a60f5201a19a22@ec2-52-207-90-231.compute-1.amazonaws.com:5432/d2veulsjfo91cj",
-    port:5432,
+const db = new Sequelize(process.env.NAME_DB, process.env.USER_DB, process.env.PASSWORD_DB, {
+    host:process.env.URL_DB,
     dialect:"postgres",
     logging: false,
 });
