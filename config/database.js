1789d6f6e4d16a6e4cd35e4564ff021ea7cb5de5
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-11-15 15:22:48 +0700

    cors v5

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize("postgres://iwjajeqcegodee:127ddf46d98b19d7c7e28ad5455779bf6f2d79ee20f53205c8867d5745e24f69@ec2-52-200-5-135.compute-1.amazonaws.com:5432/dcqn1bupdnue4m", {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
