00b3b571b3085d2b168ae9dcf68a478f28941612
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-11-15 15:37:53 +0700

    cors v6

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("postgres://iwjajeqcegodee:127ddf46d98b19d7c7e28ad5455779bf6f2d79ee20f53205c8867d5745e24f69@ec2-52-200-5-135.compute-1.amazonaws.com:5432/dcqn1bupdnue4m", {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: true
-    }
+
+const db=new Sequelize("sql12572413",'sql12572413','IdBkMKe69V',{
+    host:"sql12.freesqldatabase.com",
+    port:3306,
+    dialect:"mysql",
+    logging:false,
 });
 
 export default db; 
