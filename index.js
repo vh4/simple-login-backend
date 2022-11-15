86948ff334641f1f18fb0ba1152a4cbd48c6509e
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-11-15 16:21:21 +0700

    cors v7

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import helmet from "helmet"
 
 dotenv.config();
 const app = express();
-const port  = process.env.PORT || 3000;
+const port  = process.env.PORT || 5000;
 
 app.set('trust proxy', 1);
 app.use(cors({
