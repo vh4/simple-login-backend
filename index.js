45a44d4d297cdc005af44c7f0dc0a6b676a39cbc
Fathoni waseso jati <35059196+vh4@users.noreply.github.com>
2022-11-15 21:27:26 +0700

    cors v7

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ import Routes from "./route/api.js";
 import * as dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import helmet from "helmet"
 
 dotenv.config();
 const app = express();
 const port  = process.env.PORT || 5000;
 
-app.set('trust proxy', 1);
 app.use(cors({
     origin:["https://ap1-frontend.herokuapp.com", "http://localhost:3000"],
     methods: ['GET', 'POST','DELETE', 'PUT'],
     credentials:true
 })); 
-app.use(helmet());
 
 try {
     await db.authenticate();
